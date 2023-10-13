af785e81ded02181ffba873810d659146f4c1151
zhe.chen <zhe.chen@lbk.one>
2023-10-13 13:51:07 +0800

    注释

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //【注意】DepthBean、EtfNetValueBean这两个内部类是在导致编译失败的元凶！！！
         val wsFutureDepth = WsFutureDepth()
         val wsSpotEtf = WsSpotEtf()
         wsFutureDepth.depth = WsFutureDepth.DepthBean()
